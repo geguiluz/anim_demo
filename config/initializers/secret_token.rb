b976745e161fc8849c4a7b17bd99988ee1657529
Memo <memo.eguiluz@gmail.com>
2014-01-31 01:53:31 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnimDemo::Application.config.secret_key_base = 'ccbb59de8828862cc823771c8858aa92d9b03727a2807d7cb52947166a35b4c981f74cca55d5fbc047b776a52b3f53ba9649d41eece93f7cbe5d35444b218533'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnimDemo::Application.config.session_store :cookie_store, key: '_anim_demo_session'
